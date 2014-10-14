8e82cb2abbc185a9d20c0aebd20d68671c12985e
Albert-Jan Nijburg <albertjan@curit.com>
2014-10-14 16:45:00 +0200

    run tests from build.cmd

--- build.cmd
+++ build.cmd
@@ -9,4 +9,9 @@ IF NOT EXIST packages\FAKE\tools\FAKE.exe  (
 IF NOT EXIST build.fsx (
   packages\FAKE\tools\FAKE.exe init.fsx
 )
+
+IF NOT EXIST packages\xunit.runners\tools\xunit.console.clr4.exe (
+  .nuget\nuget.exe install xunit.runners -OutputDirectory packages -ExcludeVersion
+)
+
 packages\FAKE\tools\FAKE.exe build.fsx %*
