1060263e1715fafb681232f3002adbccc8a25ea6
Remko Boschker <remko@informatietuin.nl>
2014-10-14 15:59:27 +0200

    init

---
+++ src/oxen/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("oxen")>]
+[<assembly: AssemblyProductAttribute("oxen")>]
+[<assembly: AssemblyDescriptionAttribute("fsharp client for bull.js")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
