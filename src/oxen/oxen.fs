2b4aa540229e43272754d332ca37d86a6f225071
Albert-Jan Nijburg <albertjan@curit.com>
2014-11-17 12:30:26 +0100

    start of pubsub new job messaging

--- src/oxen/oxen.fs
+++ src/oxen/oxen.fs
@@ -4,6 +4,7 @@ open System
 open StackExchange.Redis
 open System.Threading.Tasks
 open Newtonsoft.Json
+open System.Threading
 
 [<AutoOpen>]
 module OxenConvenience = 
@@ -14,9 +15,19 @@ module OxenConvenience =
     let fromValueI64 (x:RedisValue):int64 = int64(x)
     let fromValueI32 (x:RedisValue):int = int(x)
     let valueToKeyLong (x:RedisValue):RedisKey = RedisKey.op_Implicit(int64(x).ToString ())
+
+    /// <summary>
+    /// Option coalesing operator 
+    /// <c>
+    /// None |? 4 -> 4
+    /// Some 4 |? 3 -> 4
+    /// </c>
+    /// </summary>
     let (|?) (x: 'a option) (y: 'a) =  match x with | None -> y | Some z -> z
 
     let LOCK_RENEW_TIME = 5000.0
+
+    exception JobHandleException of string
 
 module Async =
     let inline awaitPlainTask (task: Task) = 
@@ -36,6 +47,7 @@ type EventType =
     | Failed
     | Paused
     | Resumed
+    | NewJob
 
 type Job<'a> = 
     {
@@ -57,12 +69,12 @@ type Job<'a> =
         |]
     member this.progress progress = 
         async {
-            let client:IDatabase = this.queue.client
+            let client:IDatabase = this.queue.client()
             do! client.HashSetAsync (
                     this.queue.toKey(this.jobId.ToString ()), 
                     [| HashEntry (toValueStr "progress", toValueI32 progress) |]
                 ) |> Async.awaitPlainTask
-            do! this.queue.emit(EventType.Progress, this, progress)
+            do! this.queue.emitJobEvent(EventType.Progress, this, progress)
         }
     member this.remove () = async { raise (NotImplementedException ()) }
     member this.takeLock (token, ?renew) = async {
@@ -70,7 +82,7 @@ type Job<'a> =
                      | Some x -> When.NotExists
                      | None -> When.Always     
             let value = (token.ToString ()) |> toValueStr
-            let client:IDatabase = this.queue.client
+            let client:IDatabase = this.queue.client()
             return! 
                 client.StringSetAsync (
                     this.lockKey (), 
@@ -90,7 +102,7 @@ type Job<'a> =
             end \n"
 
         let! result = 
-            this.queue.client.ScriptEvaluateAsync (
+            this.queue.client().ScriptEvaluateAsync (
                 script, 
                 [|this.lockKey()|], 
                 [|(token.ToString ()) |> toValueStr|]
@@ -102,7 +114,7 @@ type Job<'a> =
             let queue = this.queue
             let activeList = queue.toKey("active")
             let dest = queue.toKey(set)
-            let client:IDatabase = queue.client
+            let client:IDatabase = queue.client()
             let multi = client.CreateTransaction()
             do! multi.ListRemoveAsync (activeList, toValueI64 this.jobId) 
                 |> Async.AwaitTask 
@@ -113,7 +125,7 @@ type Job<'a> =
         }
     member private this.isDone list =
         async {
-            let client:IDatabase = this.queue.client
+            let client:IDatabase = this.queue.client()
             return! client.SetContainsAsync(this.queue.toKey(list), toValueI64 this.jobId) |> Async.AwaitTask
         }
     member this.moveToCompleted () = this.moveToSet("completed")
@@ -124,14 +136,14 @@ type Job<'a> =
     static member create (queue, jobId, data:'a, opts) = 
         async { 
             let job = { queue = queue; data = data; jobId = jobId; opts = opts; _progress = 0 }
-            let client:IDatabase = queue.client
+            let client:IDatabase = queue.client()
             do! client.HashSetAsync (queue.toKey (jobId.ToString ()), job.toData ()) |> Async.awaitPlainTask
             return job 
         }
     static member fromId<'a> (queue: Queue<'a>, jobId: RedisKey) = 
         async {
             let client = queue.client
-            let! job = client.HashGetAllAsync (jobId) |> Async.AwaitTask
+            let! job = client().HashGetAllAsync (jobId) |> Async.AwaitTask
             //staan hash values altijd op dezelfde volgorde
             return Job.fromData (
                 queue, 
@@ -145,105 +157,186 @@ type Job<'a> =
         let sOpts = JsonConvert.DeserializeObject<Map<string, string> option>(opts)
         { queue = queue; data = sData; jobId = jobId; opts = sOpts; _progress = progress }
 
-and OxenEvent<'a> =
+and OxenJobEvent<'a> =
     {
-        job: Job<'a> option
+        job: Job<'a>
+        data: obj option
         progress: int option
         err: exn option
     }
 
+and OxenQueueEvent<'a> =
+    {
+        queue: Queue<'a>
+    }
+
 and Events<'a> = {
-    Completed: IEvent<OxenEvent<'a>>
-    Progress: IEvent<OxenEvent<'a>>
-    Failed: IEvent<OxenEvent<'a>>
-    Paused: IEvent<OxenEvent<'a>>
-    Resumed: IEvent<OxenEvent<'a>>
+    Completed: IEvent<OxenJobEvent<'a>>
+    Progress: IEvent<OxenJobEvent<'a>>
+    Failed: IEvent<OxenJobEvent<'a>>
+    Paused: IEvent<OxenQueueEvent<'a>>
+    Resumed: IEvent<OxenQueueEvent<'a>>
 }
 
-and Queue<'a> (name, db:IDatabase) as this =
-    let token = Guid.NewGuid ()
-    
-    let completedEvent = new Event<OxenEvent<'a>> ()
-    let progressEvent = new Event<OxenEvent<'a>> ()
-    let failedEvent = new Event<OxenEvent<'a>> ()
-    let pausedEvent = new Event<OxenEvent<'a>> ()
-    let resumedEvent = new Event<OxenEvent<'a>> ()
+and LockRenewer<'a> (job:Job<'a>) =
+    let cts = new CancellationTokenSource ()
 
-    let processJob job = async { 
-            // if paused then return else
-            // processing <- true; job.renewLock(); renewlocktimeout <- settimeout;
-            //try -> handler(); job.moveToCompleted; emit "completed"
-            // catch -> job.moveToFailed; job.releaseLock; emit "failed"
-            //cleartimeout(lockrenewtimeout); processing <- false 
-            ()
+    let rec lockRenewer (job:Job<'a>) = 
+        async {
+            do! job.renewLock() |> Async.Ignore
+            do! Async.Sleep (int(LOCK_RENEW_TIME / 2.0)) 
+            return! lockRenewer job
         }
-    let processStalledJob (job:Job<'a>) = 
+
+    do Async.Start (lockRenewer job, cts.Token)
+
+    interface IDisposable
+        member x.Dispose() =
+            x.Dispose(true)
+            GC.SuppressFinalize(x);
+
+    member x.Dispose(disposing) = 
+        if (disposing) then
+            cts.Cancel ()
+
+    override x.Finalize () =
+        x.Dispose(false)
+            
+/// <summary>
+/// The queue
+/// </summary>
+/// <param name="name">Name of the queue</param>
+/// <param name="connection">Redis connectionstring</param>
+and Queue<'a> (name, multiplexer:ConnectionMultiplexer) as this =
+    let mutable paused = false
+    let mutable processing = false
+
+    let dbFactory () = multiplexer.GetDatabase()
+    let multiplexer = multiplexer
+    let sub = multiplexer.GetSubscriber()
+    do async {
+        sub.SubscribeAsync(this.toChannel("jobchannel"), Action<RedisChannel, RedisValue>(fun c v -> this.emitJobEvent(NewJob, Job.fromId(this, valueToKeyLong v) |> Async.RunSynchronously) |> Async.RunSynchronously)) |> Async.awaitPlainTask |> Async.RunSynchronously
+        ()
+    } |> Async.RunSynchronously
+
+    let token = Guid.NewGuid ()
+    let completedEvent = new Event<OxenJobEvent<'a>> ()
+    let progressEvent = new Event<OxenJobEvent<'a>> ()
+    let failedEvent = new Event<OxenJobEvent<'a>> ()
+    let pausedEvent = new Event<OxenQueueEvent<'a>> ()
+    let resumedEvent = new Event<OxenQueueEvent<'a>> ()
+    let newJobEvent = new Event<OxenQueueEvent<'a>> ()
+
+    let once s = async { () }
+
+    let processJob handler job = 
+        async { 
+            if paused then ()
+            processing <- true
+            use lr = new LockRenewer<'a>(job)
+            try
+                let! data = handler job 
+                do! job.moveToCompleted () |> Async.Ignore
+                do! this.emitJobEvent(Completed, job, data = data)
+                data
+            with 
+                | _ as e -> 
+                    do! job.moveToFailed () |> Async.Ignore
+                    do! job.releaseLock () |> Async.Ignore
+                    do! this.emitJobEvent(Failed, job, exn = e) 
+        }
+
+    let processJobs handler = 
+        async {
+
+
+        }
+
+    let processStalledJob handler (job:Job<'a>) = 
         async { 
             let! lock = job.takeLock token
             match lock with
             | true -> 
                 let key = this.toKey("completed");
                 let! contains = 
-                    this.client.SetContainsAsync (key, job.jobId |> toValueI64) 
+                    this.client().SetContainsAsync (key, job.jobId |> toValueI64) 
                     |> Async.AwaitTask
+                
                 if contains then 
-                    do! processJob(job)
+                    return! processJob handler job
+                    
             | false -> ()
         }
-    let processStalledJobs () = 
+
+    let processStalledJobs handler = 
         async {
-            let! range = db.ListRangeAsync (this.toKey ("active"), 0L, -1L) |> Async.AwaitTask
-            let jobs = 
+            let! range = this.client().ListRangeAsync (this.toKey ("active"), 0L, -1L) |> Async.AwaitTask
+            let! jobs = 
                 range 
                 |> Seq.map fromValueStr
                 |> Seq.map RedisKey.op_Implicit
                 |> Seq.map (fun x -> Job.fromId (this, x))
-            return! jobs |> Async.Parallel
-        }
-    member internal x.emit (eventType, job:Job<'a>, ?value) = 
+                |> Async.Parallel
+
+            let stalledJobsHandler = processStalledJob handler
+
+            return! jobs |> Seq.map stalledJobsHandler |> Async.Parallel
+        } 
+
+    member x.``process`` (handler:(Job<'a> * unit -> unit) -> unit) = 
+        ()
+    
+    member x.run handler = 
         async {
-            let eventData = 
-                {
-                    job = Some job
-                    progress = value
-                    err = None
-                }
-            
-            match eventType with 
-            | Progress -> progressEvent.Trigger(eventData)
-            | _ -> failwith "It'sNotYetBeenImplemented"
+            let jobHandler = processJob handler
+            let! stalledJobs = processStalledJobs handler
+            do! stalledJobs |> Seq.map jobHandler
+            ()
         }
-    member x.toKey (kind:string) = RedisKey.op_Implicit ("bull:" + name + ":" + kind)
-    member x.client = db
-    member x.process (handler:(Job<'a> * unit -> unit) -> unit) = () //process is reserved for future use
+    
     member x.add (data, ?opts:Map<string,string>) = 
         async {
-            let! jobId = this.client.StringIncrementAsync (this.toKey "id") |> Async.AwaitTask
+            let! jobId = this.client().StringIncrementAsync (this.toKey "id") |> Async.AwaitTask
             let! job = Job<'a>.create (this, jobId, data, opts) 
             let key = this.toKey "wait"
             let! res = 
                 Async.AwaitTask <|
                 match opts with
-                | None -> this.client.ListLeftPushAsync (key, toValueI64 jobId) 
+                | None -> this.client().ListLeftPushAsync (key, toValueI64 jobId) 
                 | Some opts -> 
                     if opts.ContainsKey "lifo" && bool.Parse (opts.Item "lifo") 
-                    then this.client.ListRightPushAsync (key, toValueI64 jobId) 
-                    else this.client.ListLeftPushAsync (key, toValueI64 jobId) 
+                    then this.client().ListRightPushAsync (key, toValueI64 jobId) 
+                    else this.client().ListLeftPushAsync (key, toValueI64 jobId) 
 
             return job
         }
-    member x.pause () = async { raise (NotImplementedException ()) }
+
+    member x.pause () = 
+        async {
+            if paused then 
+                return paused
+            else 
+                if processing then 
+                    do! this.on.Completed |> Async.AwaitEvent |> Async.Ignore
+                
+                paused <- true
+                do! this.emitQueueEvent(Paused, this)
+                return paused
+        }
+        
     member x.resume () = async { raise (NotImplementedException ()) }
     member x.count () = async { raise (NotImplementedException ()) }
     member x.empty () = async { raise (NotImplementedException ()) }
+
+    member x.moveJob (src, dest) = this.client().ListRightPopLeftPushAsync(src, dest) |> Async.AwaitTask
     member x.getJob id = Job<'a>.fromId (this, id)
     member x.getJobs (queueType, ?isList, ?start, ?stop) =
         async {
             let key = this.toKey(queueType)
             let! jobsIds = 
                 match isList |? false with 
-                | true -> this.client.ListRangeAsync(key, (start |? 0L), (stop |? -1L)) |> Async.AwaitTask
-                | false -> this.client.SetMembersAsync(key) |> Async.AwaitTask
+                | true -> this.client().ListRangeAsync(key, (start |? 0L), (stop |? -1L)) |> Async.AwaitTask
+                | false -> this.client().SetMembersAsync(key) |> Async.AwaitTask
 
             return!
                 jobsIds
@@ -264,3 +357,29 @@ and Queue<'a> (name, db:IDatabase) as this =
         Paused = pausedEvent.Publish
         Resumed = resumedEvent.Publish
     }
+
+    //Internals
+    member internal x.toKey (kind:string) = RedisKey.op_Implicit ("bull:" + name + ":" + kind)
+    member internal x.toChannel (kind:string) = RedisChannel.op_Implicit ("bull:" + name + ":" + kind)
+    member internal x.emitQueueEvent (eventType, queue:Queue<'a>) = 
+        async {
+            match eventType with 
+            | Paused -> pausedEvent.Trigger({ queue = queue });
+            | Resumed -> resumedEvent.Trigger({ queue = queue });
+            | _ -> failwith "Not a queue event!"
+        }
+    member internal x.emitJobEvent (eventType, job:Job<'a>, ?value, ?exn, ?data) = 
+        async {
+            let eventData = 
+                {
+                    job = job
+                    progress = value
+                    err = exn
+                    data = None
+                }
+            
+            match eventType with 
+            | Progress -> progressEvent.Trigger(eventData)
+            | _ -> failwith "It'sNotYetBeenImplemented"
+        }
+    member internal x.client = dbFactory
