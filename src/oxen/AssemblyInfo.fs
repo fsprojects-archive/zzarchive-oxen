b1e50e323f30c0b1f738a4aa6cd83a878742e661
Albert-Jan Nijburg <albertjan@curit.com>
2014-12-19 09:32:57 +0100

    Bump version to 0.0.3

--- src/oxen/AssemblyInfo.fs
+++ src/oxen/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("oxen")>]
 [<assembly: AssemblyProductAttribute("oxen")>]
 [<assembly: AssemblyDescriptionAttribute("fsharp implementation of Optimalbits/bull")>]
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyVersionAttribute("0.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
 [<assembly: InternalsVisibleToAttribute("oxen.Tests")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.2"
+    let [<Literal>] Version = "0.0.3"
