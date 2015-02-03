ad18c9d0ff5bd6fd202a9d1ae88a8073053ae0dd
albertjan <albertjan@curit.com>
2015-02-03 18:37:47 +0100

    update to latests bull

--- tests/oxen.Tests/test.js
+++ tests/oxen.Tests/test.js
@@ -2,34 +2,6 @@
 var Job = require('./node_modules/bull/lib/job');
 var Promise = require('bluebird');
 
-/**
-  Adds a job to the queue.
-  @method add
-  @param data: {} Custom data to store for this job. Should be JSON serializable.
-  @param opts: JobOptions Options for this job.
-*/
-Queue.prototype.add = function(data, opts) {
-    var _this = this;
-    opts = opts || {};
-
-    // If we fail after incrementing the job id we may end having an unused
-    // id, but this should not be so harmful
-    return _this.client.incrAsync(this.toKey('id')).then(function(jobId) {
-        return Job.create(_this, jobId, data, opts).then(function(job) {
-            var key = _this.toKey('wait');
-            var channel = _this.toKey("jobs");
-            process.stdout.write(".");
-            var multi = _this.client.multi();
-            multi[(opts.lifo ? 'r' : 'l') + 'push'](key, jobId);
-            multi.publish(channel, jobId);
-            // if queue is LIFO use rpushAsync
-            return multi.execAsync().then(function() {
-                return job;
-            });
-        });
-    });
-};
-
 var messageQueue = new Queue("test-control-messages", 6379, "localhost");
 
 messageQueue.process(function(job, cb) {
